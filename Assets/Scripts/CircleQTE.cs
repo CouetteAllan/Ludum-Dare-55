94262242045f44c481d8d10e679979262c6062f2
COUETTE Allan <couetteallan@gmail.com>
2024-04-13 14:41:26 +0200

    E

--- Assets/Scripts/CircleQTE.cs
+++ Assets/Scripts/CircleQTE.cs
@@ -7,19 +7,42 @@ using UnityEngine.EventSystems;
 public class CircleQTE : MonoBehaviour, IPointerDownHandler
 { 
     [SerializeField] private TextMeshProUGUI _indexText;
+    [SerializeField] private ParticleSystem[] _particleSystemsFeedback;
+    private int _index;
+    public int Index => _index;
     private QTEManager _manager;
 
 
     public void InitCircle(int index, QTEManager manager)
     {
-        _indexText.text = index.ToString();
+        _index = index;
+        _indexText.text = (index + 1).ToString();
         _manager = manager;
     }
 
     public void OnPointerDown(PointerEventData eventData)
     {
-        //Trigger action 
+        var cam = Camera.main;
 
+        //Trigger action 
+        if (_manager.IsNextIndex(_index))
+        {
+            var particle = Instantiate(_particleSystemsFeedback[0], cam.ScreenToWorldPoint(this.transform.position + Vector3.forward * 10.0f), Quaternion.identity) ;
+            particle.Play();
+
+        }
+        else
+        {
+            var particle = Instantiate(_particleSystemsFeedback[1], cam.ScreenToWorldPoint(this.transform.position + Vector3.forward * 10.0f), Quaternion.identity);
+            particle.Play();
+        }
+
+        if (_index == 0)
+            _manager.StartDrawing(cam.ScreenToWorldPoint(this.transform.position));
+        else
+            _manager.AddPointToLine(cam.ScreenToWorldPoint(this.transform.position));
+
+        _manager.SetPreviousIndex(_index);
         //Trigger Feedback
 
         Destroy(this.gameObject);
--- Assets/Scripts/Managers/QTEManager.cs
+++ Assets/Scripts/Managers/QTEManager.cs
@@ -6,9 +6,10 @@ public class QTEManager : MonoBehaviour
 {
     [SerializeField] private GameObject _pattern;
     [SerializeField] private Canvas _canvas;
+    [SerializeField] private LineRenderer _line;
 
     private List<CircleQTE> _circles = new List<CircleQTE>();
-
+    private int _previousIndex = -1;
 
     void Awake()
     {
@@ -24,19 +25,37 @@ public class QTEManager : MonoBehaviour
     private void SpawnPattern()
     {
         int index = 0;
-        foreach(CircleQTE circle in _pattern.GetComponentsInChildren<CircleQTE>())
+        var patternSpawned = Instantiate(_pattern, _canvas.transform);
+        foreach(CircleQTE circle in patternSpawned.GetComponentsInChildren<CircleQTE>())
         {
             _circles.Add(circle);
             circle.InitCircle(index,this);
             index++;
         }
 
-        Instantiate(_pattern, _canvas.transform);
     }
+
+    public void StartDrawing(Vector2 firstPoint)
+    {
+        _line.positionCount = 1;
+        _line.SetPosition(0,firstPoint);
+    }
+
+    public void AddPointToLine(Vector2 point)
+    {
+        _line.positionCount++;
+        _line.SetPosition(_line.positionCount - 1, point);
+    }
+
+    public void SetPreviousIndex(int index)
+    {
+        _previousIndex = index;
+    }
+
+    public bool IsNextIndex(int currentIndex) => currentIndex - 1 == _previousIndex;
 
     private void OnDisable()
     {
         GameManager.OnGameStateChanged -= GameManager_OnGameStateChanged;
-
     }
 }
