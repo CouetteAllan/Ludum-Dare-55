a3f6e572dfd0bf4367c1b67dfa32281ee6e85e84
COUETTE Allan <couetteallan@gmail.com>
2024-04-13 12:26:26 +0200

    some set up

---
+++ Assets/Scripts/Managers/Audio/Sound.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.Audio;
+
+[System.Serializable]
+public class Sound
+{
+    public string name;
+
+    public AudioClip clip;
+    public AudioMixerGroup output;
+
+    [Range(0f, 1f)]
+    public float volume;
+
+    public float pitch;
+
+    public bool loop;
+
+    public bool playOnAwake;
+
+
+    [HideInInspector]
+    public AudioSource source;
+}
